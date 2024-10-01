8e952f558995185980d2635d414511168a7630e8
DavidSparker0417 <davidsparker0417@gmail.com>
2024-10-01 20:35:54 +0200

    vv

--- frontend/src/ui-config/networksConfig.ts
+++ frontend/src/ui-config/networksConfig.ts
@@ -417,10 +417,9 @@ export const networkConfigs: Record<string, BaseNetworkConfig> = {
   },
   [ChainId.bnb]: {
     name: 'Binance Smart Chain',
-    // privateJsonRPCUrl: 'https://bsc-mainnet.rpc.grove.city/v1/62b3314e123e6f00397f19ca',
-    privateJsonRPCUrl: 'https://bsc-dataseed.binance.org',
-    publicJsonRPCUrl: ['https://binance.llamarpc.com', 'wss://bsc-rpc.publicnode.com'],
-    publicJsonRPCWSUrl: 'wss://bsc-rpc.publicnode.com',
+    privateJsonRPCUrl: 'https://bsc-mainnet.rpc.grove.city/v1/62b3314e123e6f00397f19ca',
+    publicJsonRPCUrl: ['https://bsc.publicnode.com	', 'wss://bsc.publicnode.com'],
+    publicJsonRPCWSUrl: 'wss://bsc.publicnode.com',
     baseAssetSymbol: 'BNB',
     wrappedBaseAssetSymbol: 'WBNB',
     baseAssetDecimals: 18,
